274e0d698b99655241e65f7bd6bf1ad1c5bb79a8
ronni <ronni@redweb.dk>
2009-06-26 12:14:52 +0000

    correction on bug in alteration of last update

--- component/admin/plugins/redform.php
+++ component/admin/plugins/redform.php
@@ -487,7 +487,7 @@ class plgContentRedform extends JPlugin {
 								break;
 							case 'fileupload':
 								if ($submitter_id == 0) {
-								$fileupload .= "<input type=\"file\" name=\"field".$field->id.'.'.$signup."[fileupload][]\" size=\"40\" class="fileupload" id="fileupload_'.$field->cssfield.'"/>";								
+								$fileupload .= "<input type=\"file\" name=\"field".$field->id.'.'.$signup."[fileupload][]\" size=\"40\" class=\"fileupload\" id=\"fileupload_'.$field->cssfield.'\"/>";
 								}
 								break;
 						}
