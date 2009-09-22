ac52f762d43f9d7bf0c5e7219258e26ddb826126
roland <contact@csvimproved.com>
2009-09-22 10:17:50 +0000

    / Changed license information

--- component/admin/models/log.php
+++ component/admin/models/log.php
@@ -3,21 +3,20 @@
  * @version 1.0 $Id: archive.php 217 2009-06-06 20:04:26Z julien $
  * @package Joomla
  * @subpackage redEVENT
- * @copyright redEVENT (C) 2008 redCOMPONENT.com / EventList (C) 2005 - 2008 Christoph Lukes
+ * @copyright Copyright (C) 2008 redCOMPONENT.com. All rights reserved. 
  * @license GNU/GPL, see LICENSE.php
- * redEVENT is based on EventList made by Christoph Lukes from schlu.net
- * redEVENT can be downloaded from www.redcomponent.com
- * redEVENT is free software; you can redistribute it and/or
+ * redFORM can be downloaded from www.redcomponent.com
+ * redFORM is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License 2
  * as published by the Free Software Foundation.
 
- * redEVENT is distributed in the hope that it will be useful,
+ * redFORM is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with redEVENT; if not, write to the Free Software
+ * along with redFORM; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
--- component/admin/tables/mailinglists.php
+++ component/admin/tables/mailinglists.php
@@ -1,10 +1,20 @@
 <?php
 /** 
  * @copyright Copyright (C) 2008 redCOMPONENT.com. All rights reserved. 
- * @license can be read in this package of software in the file license.txt or 
- * read on http://redcomponent.com/license.txt  
- * Developed by email@recomponent.com - redCOMPONENT.com 
- *
+ * @license GNU/GPL, see LICENSE.php
+ * redFORM can be downloaded from www.redcomponent.com
+ * redFORM is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+
+ * redFORM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with redFORM; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 /* No direct access */
