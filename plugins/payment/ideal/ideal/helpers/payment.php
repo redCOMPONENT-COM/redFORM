eee68783dcb1450dcb223a2ece75ef86e3cc8d2d
julien <julien@redweb.dk>
2010-09-17 17:04:45 +0200

    / added ideal/mollie.nl payment plugin

---
+++ plugins/payment/ideal/ideal/helpers/payment.php
@@ -0,0 +1,196 @@
+<?php
+
+class PaymentIdeal {
+	
+	var $params = null;
+	
+	/**
+	 * contructor
+	 * @param object plgin params
+	 */
+	function PaymentIdeal($params)
+	{
+		$this->params = $params;
+	}
+	
+	/**
+	 * sends the payment request associated to sumbit_key to the payment service
+	 * @param string $submit_key
+	 */
+	function process($request, $return_url = null, $cancel_url = null)
+	{
+		$posted = JRequest::getVar('partner_id', 0, 'post', 'int');
+		if ($posted) {
+			return $this->_processpost($request, $return_url, $cancel_url);
+		}
+		else {
+			return $this->_buildform($request, $return_url, $cancel_url);
+		}
+		
+	}
+	
+	/**
+	 * handle the recpetion of notification
+	 * @return bool paid status
+	 */
+  function notify()
+  {
+    $mainframe = &JFactory::getApplication();
+    $db = & JFactory::getDBO();    
+    $paid = 0;
+    				
+    $submit_key = JRequest::getvar('key');
+    JRequest::setVar('submit_key', $submit_key);
+    RedformHelperLog::simpleLog('IDEAL_NOTIFICATION_RECEIVED'. ' ' . $submit_key);
+    $transaction_id = JRequest::getVar('transaction_id');
+    
+    // first check that we didn't already received the payment...
+    $db = &JFactory::getDBO();
+    $query = ' SELECT id ' 
+           . ' FROM #__rwf_payment ' 
+           . ' WHERE submit_key = ' . $db->Quote($submit_key)
+           . '   AND paid = 1 '
+           ;
+    $db->setQuery($query);
+    $res = $db->loadResult();
+    if ($res) // already paid
+    {
+    	return true;
+    }
+    
+		$ideal = new iDEAL_Payment($this->params->get('partner_id'));
+		$ideal->setTestmode($this->params->get('testmode'));
+    $res = $ideal->checkPayment($transaction_id);
+    
+		if (!$res) {
+			RedformHelperLog::simpleLog(JText::_('IDEAL_PAYMENT_ERROR'). ' / ' . $submit_key.': '.$ideal->getErrorMessage().' ('. $ideal->getErrorCode().')');
+			JError::raiseWarning(0, $ideal->getErrorMessage());
+			return false;
+		}
+		
+		$details = $this->_getSubmission($submit_key);
+		
+		if (!$ideal->getPaidStatus())
+		{
+    	RedformHelperLog::simpleLog(JText::_('IDEAL NOTIFICATION PAYMENT REFUSED'). ' / ' . $submit_key);
+	  	$this->writeTransaction($submit_key, $ideal->getInfo(), 'NOTPAID', 0);
+			return true;
+		}
+		if ($ideal->getAmount() != round($details->price*100, 2 ))
+		{
+    	RedformHelperLog::simpleLog(JText::_('IDEAL NOTIFICATION PRICE MISMATCH'). ' / ' . $submit_key);
+    	$this->writeTransaction($submit_key, JText::_('IDEAL NOTIFICATION PRICE MISMATCH')."\n".$ideal->getInfo(), 'FAILED', 0);
+    	return false;			
+		}
+    
+	  $this->writeTransaction($submit_key, $ideal->getInfo(), 'SUCCESS', 1);
+    
+    return $paid;
+  }
+  
+  function _getSubmission($submit_key)
+  {
+		// get price and currency
+		$db  = &JFactory::getDBO();
+		
+		$query = ' SELECT f.currency, SUM(s.price) AS price, s.id AS sid '
+		       . ' FROM #__rwf_submitters AS s '
+		       . ' INNER JOIN #__rwf_forms AS f ON f.id = s.form_id '
+		       . ' WHERE s.submit_key = '. $db->Quote($submit_key)
+		       . ' GROUP BY s.submit_key'
+		            ;
+		$db->setQuery($query);
+		$res = $db->loadObject();
+		return $res;
+  }
+  
+  function writeTransaction($submit_key, $data, $status, $paid)
+  {
+    $db = & JFactory::getDBO();    
+  	
+    // payment was refused
+    $query =  ' INSERT INTO #__rwf_payment (`date`, `data`, `submit_key`, `status`, `gateway`, `paid`) '
+				    . ' VALUES (NOW() '
+				    . ', '. $db->Quote($data)
+				    . ', '. $db->Quote($submit_key)
+				    . ', '. $db->Quote($status)
+				    . ', '. $db->Quote('ideal')
+				    . ', '. $db->Quote($paid)
+				    . ') ';
+    $db->setQuery($query);
+    $db->query();
+  }
+  	
+  function _buildform($request, $return_url = null, $cancel_url = null)
+  {  	
+		$document = JFactory::getDocument();
+		
+		$details = $this->_getSubmission($request->key);
+		$submit_key = $request->key;
+		require_once(JPATH_SITE.DS.'components'.DS.'com_redform'.DS.'helpers'.DS.'currency.php');
+		$currency = RedformHelperLogCurrency::getIsoNumber($details->currency);
+		
+		$ideal = new iDEAL_Payment($this->params->get('partner_id'));
+		$ideal->setTestmode($this->params->get('testmode'));
+		
+		$banks = $ideal->getBanks();
+		$options = array();
+		foreach ($banks as $id => $b)
+		{
+			$options[] = JHTML::_('select.option', $id, $b);			
+		}
+		
+		?>		
+		<form method="post" action="<?php JRoute::_('index.php?option=com_redform&controller=payment');?>">
+		<fieldset>
+		<legend><?php echo JText::_('iDeal Payment Gateway'); ?></legend>
+		<p><?php echo $request->title; ?></p>
+		<p><?php echo $details->price; ?> Euros</p>
+		<label for="bank_id"><?php echo JText::_('IDEAL_SELECT_BANK'); ?></label>
+		<?php echo JHTML::_('select.genericlist', $options, 'bank_id')?>
+		</fieldset>
+
+		<?php echo JHTML::_( 'form.token' ); ?>
+		<input type="hidden" name="partner_id" value="<?php echo $this->params->get('partner_id'); ?>">
+		<input type="hidden" name="amount" value="<?php echo round($details->price*100, 2 ); ?>">
+		<input type="hidden" name="description" value="<?php echo $request->title; ?>">
+		<input type="hidden" name="task" value="process">
+		<input type="hidden" name="key" value="<?php echo $request->uniqueid; ?>">
+		<input type="hidden" name="gw" value="ideal">
+		
+		<input type="submit" name="submit" value="<?php echo JText::_('IDEAL_PAY_VIA_IDEAL'); ?>" />
+		</form>
+		<?php 
+  }
+  
+  function _processpost($request, $return_url = null, $cancel_url = null)
+  {
+  	$app = &JFactory::getApplication();
+    // Check for request forgeries
+    JRequest::checkToken() or die( 'Invalid Token' );
+    
+    $details = $this->_getSubmission($request->key);
+		$submit_key = $request->key;
+		
+		$ideal = new iDEAL_Payment($this->params->get('partner_id'));
+		$ideal->setTestmode($this->params->get('testmode'));
+		$ideal->setProfileKey($this->params->get('profile_key'));
+		
+		$bank = JRequest::getInt('bank_id');
+		
+		$res = $ideal->createPayment($bank, 
+		                      round($details->price*100, 2 ), 
+		                      $request->title, 
+		                      JRoute::_(JURI::base() . 'index.php?option=com_redform&controller=payment&task=notify&gw=ideal&key='. $request->key),
+		                      JRoute::_(JURI::base() . 'index.php?option=com_redform&controller=payment&task=notify&gw=ideal&key='. $request->key));
+		if (!$res) {
+			RedformHelperLog::simpleLog('IDEAL_PAYMENT_ERROR'. ' for ' . $submit_key.': '.$ideal->getErrorMessage().' ('. $ideal->getErrorCode().')');
+			JError::raiseWarning(0, $ideal->getErrorMessage());
+			return false;
+		}
+		$this->writeTransaction($submit_key, $ideal->getInfo(), 'prepared', 0);
+		$app->redirect($ideal->getBankURL());
+		
+		return true;
+  }
+}
