4711eadd500c5cb0dc2c079794dca70158265b3b
julien <julien@redweb.dk>
2010-10-18 17:25:41 +0200

    / fixed error on uninstall

--- component/uninstall.redform.php
+++ component/uninstall.redform.php
@@ -29,13 +29,9 @@ function com_uninstall(){
 	$database = JFactory::getDBO();
 	JFile::delete(JPATH_SITE.DS.'plugins'.DS.'content'.DS.'redform.xml');
 	JFile::delete(JPATH_SITE.DS.'plugins'.DS.'content'.DS.'redform.php');
-	$langfiles = JFolder::files(JPATH_SITE.DS.'administrator'.DS.'components'.DS.'com_redform'.DS.'plugins'.DS.'language');
-	$basefolder = JPATH_SITE.DS.'language';
-	foreach ($langfiles as $key => $langfile) {
-		$lang = substr($langfile, 0, 5);
-		if (JFolder::exists($basefolder.DS.$lang)) {
-			JFile::delete($basefolder.DS.$lang.DS.$langfile);
-		}
+	$langfiles = JFolder::files(JPATH_SITE.DS.'administrator'.DS.'language', '.*content_redform.*', true, true);
+	foreach ((array) $langfiles as $key => $langfile) {
+		JFile::delete($langfiles);
 	}
 	
 	$query = "DELETE FROM #__plugins WHERE folder = 'content' AND element = 'redform'";
