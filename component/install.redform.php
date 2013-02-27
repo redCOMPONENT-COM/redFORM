e3df00aebed0744a50b19683897f3f278354f88c
julien <jlv@jlv-solutions.com>
2013-02-28 00:06:47 +0100

    / updating installer for 3.0

--- component/install.redform.php
+++ component/install.redform.php
@@ -23,69 +23,136 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 class com_redformInstallerScript
-{
-	/**
-	 * method to run before an install/update/uninstall method
-	 *
-	 * @return void
-	 */
-	function preflight($type, $parent) 
+{	
+    private $installed_mods             = array();
+    private $installed_plugs            = array();
+    
+    /**
+     * method to run after an install/update/uninstall method
+     *
+     * @return void
+     */
+    public function postflight($action, $parent)
 	{
-		if ($type == 'update')
-		{
-			// get db version
-			$db = &JFactory::getDbo();
-			$query = $db->getQuery(true);
-			
-			$query->select('s.version_id');
-			$query->from('#__extensions AS e');
-			$query->join('INNER', '#__schemas AS s ON s.extension_id = e.extension_id');
-			$query->where('e.element = '.$db->Quote('com_redform'));
-			$db->setQuery($query);
-			$version = $db->loadResult();
-			
-			if (version_compare("2.0.b.5.0", $version)) 
-			{
-				$fields = $db->getTableColumns('#__rwf_values');
-				if (!isset($fields['price'])) 
-				{
-					$query = ' ALTER TABLE #__rwf_values ADD '.$db->nameQuote('price') . ' DOUBLE NULL DEFAULT NULL ';
-					$db->setQuery($query);
-					try {
-						$db->query();
-					}	
-					catch (Exception $e) {
-						echo $e->getMessage();
-					}	
-				}
-			}
+		$this->installModsPlugs($parent);
+		if (count($this->installed_plugs)){
+			echo '<div>
+                          <table class="adminlist" cellspacing="1">
+                            <thead>
+                                <tr>
+                                    <th>'.JText::_('Plugin').'</th>
+                                    <th>'.JText::_('Group').'</th>
+                                    <th>'.JText::_('Status').'</th>
+                                </tr>
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <td colspan="3">&nbsp;</td>
+                                </tr>
+                            </tfoot>
+                            <tbody>';
+			foreach ($this->installed_plugs as $plugin) :
+			$pstatus    = ($plugin['upgrade']) ? JHtml::_('image','admin/tick.png', '', NULL, true) : JHtml::_('image','admin/publish_x.png', '', NULL, true);
+			echo '<tr>
+                                            <td>'.$plugin['plugin'].'</td>
+                                            <td>'.$plugin['group'].'</td>
+                                            <td style="text-align: center;">'.$pstatus.'</td>
+                                          </tr>';
+			endforeach;
+			echo '   </tbody>
+                         </table>
+                         </div>';
 		}
+		
+		if (count($this->installed_mods)){
+			echo '<div>
+                          <table class="adminlist" cellspacing="1">
+                            <thead>
+                                <tr>
+                                    <th>'.JText::_('Module').'</th>
+                                    <th>'.JText::_('Status').'</th>
+                                </tr>
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <td colspan="2">&nbsp;</td>
+                                </tr>
+                            </tfoot>
+                            <tbody>';
+			foreach ($this->installed_mods as $module) :
+			$mstatus    = ($module['upgrade']) ? JHtml::_('image','admin/tick.png', '', NULL, true) : JHtml::_('image','admin/publish_x.png', '', NULL, true);
+			echo '<tr>
+                                            <td>'.$module['module'].'</td>
+                                            <td style="text-align: center;">'.$mstatus.'</td>
+                                          </tr>';
+			endforeach;
+			echo '   </tbody>
+                         </table>
+                         </div>';
+		}
+// 		/* Install plugin */
+// 		jimport('joomla.filesystem.file');
+// 		jimport('joomla.filesystem.folder');
+// 		JFolder::copy(JPATH_SITE.DS.'administrator'.DS.'components'.DS.'com_redform'.DS.'plugins'.DS.'content_redform', JPATH_SITE.DS.'tmp'.DS.'redform_plugin', '', true);
+// 		JFile::move(JPATH_SITE.DS.'tmp'.DS.'redform_plugin'.DS.'redform.xm', JPATH_SITE.DS.'tmp'.DS.'redform_plugin'.DS.'redform.xml');
+// 		$installer = new JInstaller();
+// 		$installer->setAdapter('plugin');
+// 		if (!$installer->install(JPATH_SITE.DS.'tmp'.DS.'redform_plugin')) {
+// 			echo JText::_('COM_REDFORM_Plugin_install_failed') . $installer->getError().'<br />';
+// 		}
+// 		else {
+// 			$db = &JFactory::getDbo();
+// 			// autopublish the plugin
+// 			$query = ' UPDATE #__extensions SET enabled = 1 WHERE name = '. $db->Quote('Content - redFORM');
+// 			$db->setQuery($query);
+// 			if ($db->query()) {
+// 				echo JText::_('COM_REDFORM_Succesfully_installed_redform_content_plugin').'<br />';
+// 			}
+// 			else {
+// 				echo JText::_('COM_REDFORM_Error_publishing_redform_content_plugin').'<br />';
+// 			}
+			 
+// 		}
 	}
 	
-	public function postflight()
-	{				
-		/* Install plugin */
-		jimport('joomla.filesystem.file');
-		jimport('joomla.filesystem.folder');
-		JFolder::copy(JPATH_SITE.DS.'administrator'.DS.'components'.DS.'com_redform'.DS.'plugins'.DS.'content_redform', JPATH_SITE.DS.'tmp'.DS.'redform_plugin', '', true);
-		JFile::move(JPATH_SITE.DS.'tmp'.DS.'redform_plugin'.DS.'redform.xm', JPATH_SITE.DS.'tmp'.DS.'redform_plugin'.DS.'redform.xml');
+	protected function installModsPlugs($parent)
+	{
+		$manifest       = $parent->get("manifest");
+		$parent         = $parent->getParent();
+		$source         = $parent->getPath("source");
+	
+		//**********************************************************************
+		// DO THIS IF WE DECIDE TO AUTOINSTALL PLUGINS/MODULES
+		//**********************************************************************
+		// install plugins and modules
 		$installer = new JInstaller();
-		$installer->setAdapter('plugin');
-		if (!$installer->install(JPATH_SITE.DS.'tmp'.DS.'redform_plugin')) {
-			echo JText::_('COM_REDFORM_Plugin_install_failed') . $installer->getError().'<br />';
+	
+		// Install plugins
+		foreach($manifest->plugins->plugin as $plugin) {
+			$attributes                 = $plugin->attributes();
+			$plg                        = $source . '/' . $attributes['folder'].'/'.$attributes['plugin'];
+// 			echo '<pre>';print_r($plg); echo '</pre>';exit;
+			$new                        = ($attributes['new']) ? '&nbsp;(<span class="green">New in v.'.$attributes['new'].'!</span>)' : '';
+			if($installer->install($plg)){
+				$this->installed_plugs[]    = array('plugin' => $attributes['plugin'].$new, 'group'=> $attributes['group'], 'upgrade' => true);
+			}else{
+				$this->installed_plugs[]    = array('plugin' => $attributes['plugin'], 'group'=> $attributes['group'], 'upgrade' => false);
+				$this->iperror[] = JText::_('Error installing plugin').': '.$attributes['plugin'];
+			}
 		}
-		else {
-			$db = &JFactory::getDbo();
-			// autopublish the plugin
-			$query = ' UPDATE #__extensions SET enabled = 1 WHERE name = '. $db->Quote('Content - redFORM');
-			$db->setQuery($query);
-			if ($db->query()) {
-				echo JText::_('COM_REDFORM_Succesfully_installed_redform_content_plugin').'<br />';
+		return true;
+	
+		// Install modules
+		foreach($manifest->modules->module as $module) {
+			$attributes             = $module->attributes();
+			$mod                    = $source . '/' . $attributes['folder'].'/'.$attributes['module'];
+			$new                    = ($attributes['new']) ? '&nbsp;(<span class="green">New in v.'.$attributes['new'].'!</span>)' : '';
+			if($installer->install($mod)){
+				$this->installed_mods[] = array('module' => $attributes['module'].$new, 'upgrade' => true);
+			}else{
+				$this->installed_mods[] = array('module' => $attributes['module'], 'upgrade' => false);
+				$this->iperror[] = JText::_('Error installing module').': '.$attributes['module'];
 			}
-			else {
-				echo JText::_('COM_REDFORM_Error_publishing_redform_content_plugin').'<br />';
-			}
-			 
 		}
 	}
 }
