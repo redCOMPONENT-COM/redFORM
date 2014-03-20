d005c8171b30d03e9283543c63d2a5ae72ffb6c7
julien <jlv@jlv-solutions.com>
2014-03-20 18:54:04 +0100

    minimalist route helper

---
+++ component/libraries/redform/helper/route.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package     Redform.Libraries
+ * @subpackage  Helper
+ *
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Class RedformHelperRoute
+ *
+ * @package     Redform.Libraries
+ * @subpackage  Helper
+ * @since       2.5
+ */
+class RedformHelperRoute
+{
+	public static function getPaymentRoute($submit_key)
+	{
+		$parts = array( "option" => "com_redform",
+			"controller"   => 'payment',
+			"task"   => 'select',
+			"key"   => $submit_key,
+		);
+
+		return self::buildUrl($parts);
+	}
+
+	/**
+	 * Returns the route from parts
+	 *
+	 * @param   array  $parts  segments of the route
+	 *
+	 * @return string
+	 */
+	protected static function buildUrl($parts)
+	{
+		return 'index.php?' . JURI::buildQuery($parts);
+	}
+}
