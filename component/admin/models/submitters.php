9160b342e9d49a82fc69438f62dc9f5e9723af36
julien <jlv@jlv-solutions.com>
2014-04-01 20:00:45 +0200

    #AT-137 Date filter on redform submitters list

--- component/admin/models/submitters.php
+++ component/admin/models/submitters.php
@@ -1,342 +1,318 @@
 <?php
 /**
- * @copyright Copyright (C) 2008 redCOMPONENT.com. All rights reserved.
- * @license GNU/GPL, see LICENSE.php
- * redFORM can be downloaded from www.redcomponent.com
- * redFORM is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 2
- * as published by the Free Software Foundation.
-
- * redFORM is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with redFORM; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * @package     Redform
+ * @subpackage  front,view
+ *
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
  */
 
-defined( '_JEXEC' ) or die( 'Direct Access to this location is not allowed.' );
+defined('_JEXEC') or die;
 
-jimport( 'joomla.application.component.model' );
+jimport('joomla.application.component.modellist');
 
 /**
+ * Class RedformModelSubmitters
+ *
+ * @package     Redform
+ * @subpackage  front,models
+ * @since       1.0
  */
-class RedformModelSubmitters extends JModel {
-
-	protected $_data = null;
-
-	/** @var integer Total entries */
-	protected $_total = null;
-
-	/** @var integer pagination limit starter */
-	protected $_limitstart = null;
-
-	/** @var integer pagination limit */
-	protected $_limit = null;
-
+class RedformModelSubmitters extends JModelList
+{
 	/**
-	 * Show all orders for which an invitation to fill in
-	 * a testimonal has been sent
+	 * Constructor.
+	 *
+	 * @param   array  $config  An optional associative array of configuration settings.
 	 */
-	function getSubmitters()
+	public function __construct($config = array())
 	{
-		if (empty($this->_data))
+
+		if (empty($config['filter_fields']))
 		{
-			$form_id = JRequest::getVar('form_id', 0);
-			$xref = JRequest::getVar('xref', JRequest::getVar('filter', false));
-			$integration = JRequest::getVar('integration', 0);
-
-      if ($form_id && $form_id > 0)
-      {
-				$db = JFactory::getDBO();
-
-				$query = $this->_buildSubmittersQuery();
-
-				$query .= " ORDER BY s.submission_date DESC ";
-
-				$db->setQuery($query,  $this->_limitstart, $this->_limit);
-
-				if ($db->getErrorNum() > 0) {
-					$this->_data = array();
-				}
-
-				$this->_data = $db->loadObjectList();
-      }
-      else {
-      	$this->_data = array();
-      }
+			$config['filter_fields'] = array(
+				'id', 's.id',
+				'form_id', 's.form_id',
+				'date', 's.date',
+			);
 		}
-		return $this->_data;
+
+		parent::__construct($config);
 	}
 
-	function _buildSubmittersQuery()
+	public function setContext($context)
 	{
-		$xref = JRequest::getVar('xref', JRequest::getVar('filter', false));
-		$form_id = JRequest::getVar('form_id', 0);
-		$integration = JRequest::getVar('integration', 0);
+		$this->context = $context;
+	}
 
-		$query =  ' SELECT s.submission_date, s.form_id, s.id AS sid, f.formname, u.*, s.price, s.currency, s.submit_key, p.status, p.paid, s.integration, s.xref '
-		        . ($integration === 'redevent' ? ', r.id as attendee_id ': '')
-						. ' FROM '.$this->_db->nameQuote('#__rwf_submitters').' AS s '
-						. ' INNER JOIN ' . $this->_db->nameQuote('#__rwf_forms').' AS f ON s.form_id = f.id '
-						. ' INNER JOIN ' . $this->_db->nameQuote('#__rwf_forms_'.$form_id) . ' AS u ON s.answer_id = u.id '
-            . ' LEFT JOIN (SELECT MAX(id) as id, submit_key FROM #__rwf_payment GROUP BY submit_key) AS latest_payment ON latest_payment.submit_key = s.submit_key'
-            . ' LEFT JOIN #__rwf_payment AS p ON p.id = latest_payment.id '
-		        . ($integration === 'redevent' ? ' INNER JOIN #__redevent_register AS r ON r.submit_key = s.submit_key ': '')
-						;
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @param   string  $ordering   An optional ordering field.
+	 * @param   string  $direction  An optional direction (asc|desc).
+	 *
+	 * @return  void
+	 */
+	protected function populateState($ordering = null, $direction = null)
+	{
+		$formId = $this->getUserStateFromRequest($this->option . '.filter.formId', 'form_id', $this->getDefaultFormId(), 'int');
+		$this->setState('form_id', $formId);
 
-		$where = array();
-		$where[] = "s.form_id = ".$form_id;
-		if ($xref && $xref > 0) {
-			$where[] = "s.xref = ".$xref;
+		$filter_from = $this->getUserStateFromRequest($this->option . '.filter.from', 'filter_from', null, 'string');
+		$this->setState('filter.from', $filter_from);
+
+		$filter_to = $this->getUserStateFromRequest($this->option . '.filter.to', 'filter_to', null, 'string');
+		$this->setState('filter.to', $filter_to);
+
+		// Load the parameters.
+		$params = JComponentHelper::getParams($this->option);
+		$this->setState('params', $params);
+
+		// List state information.
+		parent::populateState('s.submission_date', 'desc');
+	}
+
+	/**
+	 * Method to get a store id based on model configuration state.
+	 *
+	 * This is necessary because the model is used by the component and
+	 * different modules that might need different sets of data or different
+	 * ordering requirements.
+	 *
+	 * @param   string  $id  A prefix for the store id.
+	 *
+	 * @return string A store id.
+	 */
+	protected function getStoreId($id = '')
+	{
+		// Compile the store id.
+		$id	.= ':'.$this->getState('form_id');
+		$id	.= ':'.$this->getState('filter.from');
+		$id	.= ':'.$this->getState('filter.to');
+
+		return parent::getStoreId($id);
+	}
+
+	/**
+	 * Method to get a JDatabaseQuery object for retrieving the data set from a database.
+	 *
+	 * @return  JDatabaseQuery   A JDatabaseQuery object to retrieve the data set.
+	 *
+	 * @since   11.1
+	 */
+	protected function getListQuery()
+	{
+		$form_id = $this->getState('form_id');
+
+		$subPayment = "SELECT MAX(id) as id, submit_key FROM #__rwf_payment GROUP BY submit_key";
+
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('s.submission_date, s.form_id, s.id AS sid, f.formname, s.price, s.currency, s.submit_key');
+		$query->select('s.integration, s.xref');
+		$query->select('f.formname');
+		$query->select('p.status, p.paid');
+		$query->select('u.*');
+		$query->from('#__rwf_submitters AS s');
+		$query->join('INNER', '#__rwf_forms AS f ON s.form_id = f.id');
+		$query->join('INNER', '#__rwf_forms_' . $form_id . ' AS u ON s.answer_id = u.id');
+		$query->join('LEFT', '(' . $subPayment . ') AS latest_payment ON latest_payment.submit_key = s.submit_key');
+		$query->join('LEFT', '#__rwf_payment AS p ON p.id = latest_payment.id');
+		$query->where("s.form_id = " . $form_id);
+
+		if ($from = $this->getState('filter.from'))
+		{
+			$date = JFactory::getDate($from)->toSql();
+			$query->where('s.submission_date >= ' . $db->quote($date));
 		}
-		if (count($where)) {
-			$query .= ' WHERE ' . implode(' AND ', $where);
+
+		if ($to = $this->getState('filter.to'))
+		{
+			$date = JFactory::getDate($to)->toSql();
+			$query->where('s.submission_date <= ' . $db->quote($date));
 		}
+
+		// Add the list ordering clause.
+		$orderCol = $this->state->get('list.ordering', 's.submission_date');
+		$orderDirn = $this->state->get('list.direction', 'desc');
+		$query->order($db->escape($orderCol . ' ' . $orderDirn));
+
 		return $query;
 	}
 
-  function getFormsOptions()
-  {
-    $query = "SELECT id AS value, formname AS text FROM #__rwf_forms";
-    $this->_db->setQuery($query);
-    return $this->_db->loadObjectList();
-  }
+	public function getFormsOptions()
+	{
+		$query = "SELECT id AS value, formname AS text FROM #__rwf_forms";
+		$this->_db->setQuery($query);
 
+		return $this->_db->loadObjectList();
+	}
 
-  function getForm($id = null)
-  {
-  	if ($id == null) {
-  		$id = JRequest::getInt('form_id', 0);
-  	}
-  	if ($id) {
-	    $query = ' SELECT id, formname, activatepayment, currency FROM #__rwf_forms WHERE id = ' . $this->_db->Quote($id);
-	    $this->_db->setQuery($query);
-	    return $this->_db->loadObject();
-  	}
-  	else {
-  		return false;
-  	}
-  }
+	protected function getDefaultFormId()
+	{
+		$options = $this->getFormsOptions();
+
+		if (!$options)
+		{
+			return 0;
+		}
+
+		return $options[0]->value;
+	}
+
+	public function getForm($id = null)
+	{
+		if ($id == null)
+		{
+			$id = $this->getState('form_id');
+		}
+
+		if ($id)
+		{
+			$query = ' SELECT id, formname, activatepayment, currency FROM #__rwf_forms WHERE id = ' . $this->_db->Quote($id);
+			$this->_db->setQuery($query);
+
+			return $this->_db->loadObject();
+		}
+		else
+		{
+			return false;
+		}
+	}
 
 	/**
 	 * Show all orders for which an invitation to fill in
 	 * a testimonal has been sent
 	 */
-	function getSubmittersExport()
+	public function getSubmittersExport()
 	{
 		$form_id = JRequest::getVar('form_id', false);
 		$xref = JRequest::getVar('xref', JRequest::getVar('filter', false));
 
 		$db = JFactory::getDBO();
 		$query = ' SELECT s.submission_date, s.price, f.formname, u.*, s.price, p.status, p.paid '
-		       . ' FROM #__rwf_submitters AS s '
-		       . ' INNER JOIN #__rwf_forms AS f ON s.form_id = f.id '
-		       . ' INNER JOIN #__rwf_forms_'.$form_id.' AS u ON s.answer_id = u.id '
-		       . ' LEFT JOIN #__rwf_payment AS p ON p.submit_key = s.submit_key'
-		       ;
+			. ' FROM #__rwf_submitters AS s '
+			. ' INNER JOIN #__rwf_forms AS f ON s.form_id = f.id '
+			. ' INNER JOIN #__rwf_forms_' . $form_id . ' AS u ON s.answer_id = u.id '
+			. ' LEFT JOIN #__rwf_payment AS p ON p.submit_key = s.submit_key';
 		$where = array();
-		if ($form_id && $form_id > 0) {
-			$where[] = "s.form_id = ".$form_id;
+		if ($form_id && $form_id > 0)
+		{
+			$where[] = "s.form_id = " . $form_id;
 		}
-		if ($xref && $xref > 0) {
-			$where[] = "s.xref = ".$xref;
+		if ($xref && $xref > 0)
+		{
+			$where[] = "s.xref = " . $xref;
 		}
-		if (count($where)) {
-			$query .= ' WHERE '. implode(' AND ', $where);
+		if (count($where))
+		{
+			$query .= ' WHERE ' . implode(' AND ', $where);
 		}
 		$query .= " ORDER BY s.submission_date ASC ";
 		$db->setQuery($query);
 		return $db->loadObjectList();
 	}
 
-	function getFields()
+	public function getFields()
 	{
 		$db = JFactory::getDBO();
 		$form_id = JRequest::getInt('form_id', false);
+
 		$query = ' SELECT f.id, f.field '
-		       . '      , CASE WHEN (CHAR_LENGTH(f.field_header) > 0) THEN f.field_header ELSE f.field END AS field_header '
-		       . ' FROM #__rwf_fields AS f '
-		       . ' WHERE f.fieldtype <> "info" '
-		       ;
-		if ($form_id) $query .= ' AND form_id = ' . $db->Quote($form_id);
+			. '      , CASE WHEN (CHAR_LENGTH(f.field_header) > 0) THEN f.field_header ELSE f.field END AS field_header '
+			. ' FROM #__rwf_fields AS f '
+			. ' WHERE f.fieldtype <> "info" ';
+
+		if ($form_id)
+		{
+			$query .= ' AND form_id = ' . $db->Quote($form_id);
+		}
+
 		$query .= "GROUP BY f.id
 				ORDER BY f.ordering ";
+
 		$db->setQuery($query);
+
 		return $db->loadObjectList();
-	}
-
-	function getPagination()
-	{
-		$mainframe = JFactory::getApplication();
-		$option = JRequest::getVar('option');
-
-		/* Lets load the pagination if it doesn't already exist */
-		jimport('joomla.html.pagination');
-		$this->_limit      = $mainframe->getUserStateFromRequest( 'global.list.limit', 'limit', $mainframe->getCfg('list_limit'), 'int' );
-		$this->_limitstart = $mainframe->getUserStateFromRequest( $option.'.limitstart', 'limitstart', 0, 'int' );
-		$this->_pagination = new JPagination( $this->getTotal(), $this->_limitstart, $this->_limit );
-		return $this->_pagination;
-	}
-
-  /**
-   * Method to get the total number of items return by the query
-   *
-   * @access public
-   * @return integer
-   */
-  function getTotal()
-  {
-  	if (!JRequest::getInt('form_id')) {
-  		return 0;
-  	}
-    // Lets load the content if it doesn't already exist
-    if (empty($this->_total))
-    {
-      $this->_total = $this->_getListCount($this->_buildSubmittersQuery());
-    }
-
-    return $this->_total;
-  }
-
-	/**
-	 * Get the number of people signed up for the newsletter
-	 */
-	public function getNewsletterSignup() {
-		$db = JFactory::getDBO();
-		$cid = JRequest::getVar('cid');
-		$query = "SELECT COUNT(id)
-				FROM #__rwf_submitters
-				WHERE form_id = ".$cid[0]."
-				AND submitternewsletter = 1";
-		$db->setQuery($query);
-		if ($db->getErrorNum() > 0) return 0;
-		else return $db->loadResult();
 	}
 
 	/**
 	 * Deletes one or more submitters
 	 *
-	 * @param array id of submitters records to delete
+	 * @param array   id of submitters records to delete
 	 * @param boolean force deletion of integration rows
 	 */
-   function delete($cid, $force = false)
-   {
-      $mainframe = JFactory::getApplication();
-      $database = JFactory::getDBO();
-      JArrayHelper::toInteger( $cid );
-
-      if (!is_array( $cid ) || count( $cid ) < 1) {
-         $mainframe->enqueueMessage(JText::_('COM_REDFORM_No_submitter_found_to_delete'));
-         return false;
-      }
-
-      if (count($cid))
-      {
-      	$cids = ' s.id IN (' . implode( ',', $cid ) .') ';
-
-      	// first, check that there is no integration (xref is then > 0) among these 'submitter'
-      	if (!$force)
-      	{
-	      	$query = ' SELECT COUNT(*) FROM #__rwf_submitters AS s WHERE ' . $cids . ' AND s.xref > 0 ';
-	        $database->setQuery( $query );
-	        $res = $database->loadResult();
-	        if ($res)
-	        {
-	        	$msg = JText::_('COM_REDFORM_CANNOT_DELETE_REDEVENT_REGISTRATION');
-	        	$this->setError($msg);
-	        	JError::raiseWarning(0, $msg);
-	        	return false;
-	        }
-      	}
-
-      	// first delete the answers
-      	$query = ' DELETE a.* '
-      	       . ' FROM #__rwf_submitters AS s '
-      	       . ' INNER JOIN #__rwf_forms_'.JRequest::getInt('form_id').' AS a ON s.answer_id = a.id '
-      	       . ' WHERE ' . $cids;
-      	$this->_db->setQuery($query);
-      	$res = $this->_db->loadObjectList();
-
-      	if (!$database->query())
-      	{
-      		$mainframe->enqueueMessage(JText::_('COM_REDFORM_A_problem_occured_when_deleting_the_answers'));
-          RedformHelperLog::simpleLog(JText::_('COM_REDFORM_A_problem_occured_when_deleting_the_answers') . ': ' . $database->getErrorMsg());
-          return false;
-      	}
-
-      	/* then delete the submitters */
-      	$query = ' DELETE s.* FROM #__rwf_submitters AS s '
-      	      . ' WHERE ' . $cids
-      	      . '	AND s.form_id = '.JRequest::getInt('form_id');
-      	$database->setQuery( $query );
-
-      	if (!$database->query())
-      	{
-      		$mainframe->enqueueMessage(JText::_('COM_REDFORM_A_problem_occured_when_deleting_the_submitter'));
-          RedformHelperLog::simpleLog(JText::_('COM_REDFORM_A_problem_occured_when_deleting_the_submitter') . ': ' . $database->getErrorMsg());
-          return false;
-      	}
-
-      	if (count($cid) > 1) {
-      		$mainframe->enqueueMessage(JText::_('COM_REDFORM_Submitters_have_been_deleted'));
-      	}
-      	else {
-      		$mainframe->enqueueMessage(JText::_('COM_REDFORM_Submitter_has_been_deleted'));
-      	}
-      }
-      return JText::_('COM_REDFORM_Removal_succesfull');
-   	}
-
-	/**
-	 * Get the value for the submitter field
-	 */
-	private function QuestionValue(&$value_id) {
-		$db = JFactory::getDBO();
-		$q = "SELECT value
-			FROM #__rwf_values
-			WHERE id = ".$value_id;
-		$db->setQuery($q);
-		return $db->loadResult();
-	}
-
-	/**
-	 * Get the course
-	 */
-	public function getCourse()
+	public function delete($cid, $force = false)
 	{
-		$db = JFactory::getDBO();
-		$xref = JRequest::getVar('xref', JRequest::getVar('filter', false), 'int');
+		$mainframe = JFactory::getApplication();
+		$database = JFactory::getDBO();
+		JArrayHelper::toInteger($cid);
 
-		if (!$xref) {
+		if (!is_array($cid) || count($cid) < 1)
+		{
+			$mainframe->enqueueMessage(JText::_('COM_REDFORM_No_submitter_found_to_delete'));
 			return false;
 		}
-		$q =  ' SELECT e.title, v.venue, dates, enddates, times, endtimes, e.course_code '
-		    . ' FROM #__redevent_event_venue_xref x '
-		    . '	LEFT JOIN #__redevent_events e ON e.id = x.eventid '
-		    . ' LEFT JOIN #__redevent_venues v ON v.id = x.venueid '
-		    . ' WHERE x.id = '.$xref
-		    ;
-		$db->setQuery($q);
-		$course = $db->loadObject();
-		$course_title = $course->title . ' / ' . $course->venue . ' / ' . $course->dates;
-    if ($course->times) {
-      $course_title .= ' ' . $course->times;
-    }
-    if ($course->enddates || $course->endtimes) {
-      $course_title .= ' -';
-    }
-		if ($course->enddates) {
-			$course_title .= ' ' . $course->enddates;
+
+		if (count($cid))
+		{
+			$cids = ' s.id IN (' . implode(',', $cid) . ') ';
+
+			// first, check that there is no integration (xref is then > 0) among these 'submitter'
+			if (!$force)
+			{
+				$query = ' SELECT COUNT(*) FROM #__rwf_submitters AS s WHERE ' . $cids . ' AND CHAR_LENGTH(s.integration) > 0 ';
+				$database->setQuery($query);
+				$res = $database->loadResult();
+				if ($res)
+				{
+					$msg = JText::_('COM_REDFORM_CANNOT_DELETE_INTEGRATION_SUBMISSION');
+					$this->setError($msg);
+					JError::raiseWarning(0, $msg);
+					return false;
+				}
+			}
+
+			// first delete the answers
+			$query = ' DELETE a.* '
+				. ' FROM #__rwf_submitters AS s '
+				. ' INNER JOIN #__rwf_forms_' . JRequest::getInt('form_id') . ' AS a ON s.answer_id = a.id '
+				. ' WHERE ' . $cids;
+			$this->_db->setQuery($query);
+			$res = $this->_db->loadObjectList();
+
+			if (!$database->query())
+			{
+				$mainframe->enqueueMessage(JText::_('COM_REDFORM_A_problem_occured_when_deleting_the_answers'));
+				RedformHelperLog::simpleLog(JText::_('COM_REDFORM_A_problem_occured_when_deleting_the_answers') . ': ' . $database->getErrorMsg());
+				return false;
+			}
+
+			/* then delete the submitters */
+			$query = ' DELETE s.* FROM #__rwf_submitters AS s '
+				. ' WHERE ' . $cids
+				. '	AND s.form_id = ' . JRequest::getInt('form_id');
+			$database->setQuery($query);
+
+			if (!$database->query())
+			{
+				$mainframe->enqueueMessage(JText::_('COM_REDFORM_A_problem_occured_when_deleting_the_submitter'));
+				RedformHelperLog::simpleLog(JText::_('COM_REDFORM_A_problem_occured_when_deleting_the_submitter') . ': ' . $database->getErrorMsg());
+				return false;
+			}
+
+			if (count($cid) > 1)
+			{
+				$mainframe->enqueueMessage(JText::_('COM_REDFORM_Submitters_have_been_deleted'));
+			}
+			else
+			{
+				$mainframe->enqueueMessage(JText::_('COM_REDFORM_Submitter_has_been_deleted'));
+			}
 		}
-    if ($course->endtimes) {
-      $course_title .= ' ' . $course->endtimes;
-    }
-		$course->course_title = $course_title;
-		$course->uniqueid_prefix = $course->course_code.'-'.$xref.'-';
-		return $course;
+		return JText::_('COM_REDFORM_Removal_succesfull');
 	}
 }
-?>
--- component/admin/views/submitters/view.html.php
+++ component/admin/views/submitters/view.html.php
@@ -1,108 +1,101 @@
 <?php
-/** 
- * @copyright Copyright (C) 2008 redCOMPONENT.com. All rights reserved. 
- * @license GNU/GPL, see LICENSE.php
- * redFORM can be downloaded from www.redcomponent.com
- * redFORM is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 2
- * as published by the Free Software Foundation.
-
- * redFORM is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with redFORM; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+/**
+ * @package     Redform
+ * @subpackage  front,view
+ *
+ * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
  */
 
-/* No direct access */
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-jimport( 'joomla.application.component.view' );
+jimport('joomla.application.component.view');
 
 /**
- * redFORM View
+ * Class RedformViewSubmitters
+ *
+ * @package     Redform
+ * @subpackage  front,views
+ * @since       1.0
  */
-class RedformViewSubmitters extends JView {
-	
-  function display($tpl = null) 
-  {
-  	$params = JComponentHelper::getParams('com_redform');
-  	/* Get the forms */  
-  	$forms = $this->get('FormsOptions');
-  
-  	// set the menu
-  	RedformHelper::setMenu();
-    	
-  	if (empty($forms)) {
-  		echo '<p>'.JText::_('COM_REDFORM_SUBMITTERS_NO_FORM').'</p>';
-  		return;
-  	}
-  	
-  	// we need to chose a form by default, for the database queries (form table names...)
-  	$id = JRequest::getVar('form_id', false);
-  	if (!$id && isset($forms[0])) {
-  		JRequest::setVar('form_id', $forms[0]->value);
-  	}
-        
-  	/* Create the dropdown list */
-  	$lists['form_id'] = JHTML::_('select.genericlist',  $forms, 'form_id', '', 'value', 'text', JRequest::getVar('form_id'));
+class RedformViewSubmitters extends JView
+{
+	/**
+	 * Execute and display a template script.
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  mixed  A string if successful, otherwise a JError object.
+	 */
+	public function display($tpl = null)
+	{
+		$app = JFactory::getApplication();
+		$state = $this->get('State');
 
-  	/* Get the form name, if one is selected */
-  	$form = $this->get('Form');
-  	$this->assignRef('form', $form);
+		$params = $state->get('params');
+		$this->assignRef('params', $params);
 
-  	/* Get the pagination */
-  	$pagination = $this->get('Pagination');
+		/* Get the forms */
+		$forms = $this->get('FormsOptions');
 
-  	/* Get the submitters list */
-  	$submitters = $this->get('Submitters');
+		// Set the menu
+		RedformHelper::setMenu();
 
-  	/* Get the fields list */
-  	$fields = $this->get('Fields');
+		if (empty($forms))
+		{
+			echo '<p>' . JText::_('COM_REDFORM_SUBMITTERS_NO_FORM') . '</p>';
 
-  	/* Get the event details if there is an xref value */
-  	if (JRequest::getInt('xref', false)) { // check integration too !
-  		$course = $this->get('Course');
-  		$coursetitle = $course->course_title;
-  	}
-  	else {
-  		$course      = null;
-  		$coursetitle = null;
-  	}
+			return;
+		}
 
-  	/* Set variabels */
-  	$this->assignRef('pagination',  $pagination);
-  	$this->assignRef('submitters',  $submitters);
-  	$this->assignRef('lists',       $lists);
-  	$this->assignRef('fields',      $fields);
-  	$this->assignRef('coursetitle', $coursetitle);
-  	$this->assignRef('course',      $course);
-    $this->assignRef('xref',        JRequest::getInt('xref', 0));
-    $this->assignRef('integration', JRequest::getVar('integration', ''));
-    $this->assignRef('params',      $params);
+		/* Create the dropdown list for form filter */
+		$form_id = $state->get('form_id');
+		$lists['form_id'] = JHTML::_('select.genericlist', $forms, 'form_id', '', 'value', 'text', $form_id);
 
-  	JToolBarHelper::title(JText::_('COM_REDFORM_Submitters' ), 'redform_submitters');
-  	if (JRequest::getVar('xref', false)) JToolBarHelper::back();
-  	JToolBarHelper::deleteList(JText::_('COM_REDFORM_SUBMITTERS_DELETE_WARNING'));
-  	if ($this->params->get('showintegration', false)) {
-  		JToolBarHelper::custom('forcedelete', 'delete', 'delete',JText::_('COM_REDFORM_Force_delete'), true);
-  	}
-  	
-  	//TODO: fix the add/modify submitters from backend
-  	JToolBarHelper::editListX();
-  	if (JRequest::getVar('xref', false)) JToolBarHelper::addNewX();
+		/* Get the form name, if one is selected */
+		$form = $this->get('Form');
+		$this->assignRef('form', $form);
+
+		/* Get the pagination */
+		$pagination = $this->get('Pagination');
+
+		/* Get the submitters list */
+		$submitters = $this->get('Items');
+
+		/* Get the fields list */
+		$fields = $this->get('Fields');
+
+		$filter_from = JHTML::_('calendar', $state->get('filter.from'), 'filter_from', 'filter_from');
+		$filter_to = JHTML::_('calendar', $state->get('filter.to'), 'filter_to', 'filter_to');
+
+		/* Set variabels */
+		$this->assignRef('pagination', $pagination);
+		$this->assignRef('submitters', $submitters);
+		$this->assignRef('lists', $lists);
+		$this->assignRef('fields', $fields);
+		$this->assignRef('integration', $app->input->get('integration', ''));
+		$this->assignRef('filter_from', $filter_from);
+		$this->assignRef('filter_to', $filter_to);
+
+		JToolBarHelper::title(JText::_('COM_REDFORM_Submitters'), 'redform_submitters');
+
+		JToolBarHelper::deleteList(JText::_('COM_REDFORM_SUBMITTERS_DELETE_WARNING'));
+
+		if ($params->get('showintegration', false))
+		{
+			JToolBarHelper::custom('forcedelete', 'delete', 'delete', JText::_('COM_REDFORM_Force_delete'), true);
+		}
+
+		JToolBarHelper::editList();
+
 		JToolBarHelper::divider();
+
 		if (JFactory::getUser()->authorise('core.admin', 'com_redform'))
 		{
 			JToolBarHelper::preferences('com_redform');
 		}
-  	
-  	/* Display the page */
-  	parent::display($tpl);
-  }
-  
+
+		/* Display the page */
+		parent::display($tpl);
+	}
 }
-?>
