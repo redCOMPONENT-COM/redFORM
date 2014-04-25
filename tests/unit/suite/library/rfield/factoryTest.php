4b1ddafaeaef1283390ad26173c98aad969041ce
julien <jlv@jlv-solutions.com>
2014-04-25 17:44:28 +0200

    update tests

--- tests/unit/suite/library/rfield/factoryTest.php
+++ tests/unit/suite/library/rfield/factoryTest.php
@@ -10,6 +10,7 @@
 require_once 'PHPUnit/Autoload.php';
 
 // Register library prefix
+JLoader::registerPrefix('R', JPATH_LIBRARIES . '/redcore');
 RLoader::registerPrefix('Rdf', JPATH_LIBRARIES . '/redform');
 
 /**
