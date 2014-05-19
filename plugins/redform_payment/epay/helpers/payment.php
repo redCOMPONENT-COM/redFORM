52e085f3bdd3e48732f3f4cd1b58692fcb618184
julien <jlv@jlv-solutions.com>
2014-05-19 19:03:08 +0200

    #REDFORM-16 Redesigned epay integration

--- plugins/redform_payment/epay/helpers/payment.php
+++ plugins/redform_payment/epay/helpers/payment.php
@@ -1,7 +1,9 @@
 <?php
 /**
- * @copyright Copyright (C) 2008-2013 redCOMPONENT.com. All rights reserved.
- * @license GNU/GPL, see LICENSE.php
+ * @package     Redform
+ * @subpackage  Payment.epay
+ * @copyright   Copyright (C) 2008 redCOMPONENT.com. All rights reserved.
+ * @license     GNU/GPL, see LICENSE.php
  * redFORM can be downloaded from www.redcomponent.com
  * redFORM is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License 2
@@ -18,16 +20,16 @@
  *
  */
 
-/**
- */
-// no direct access
 defined('_JEXEC') or die('Restricted access');
 
 /**
- * @package  RED.redform
- * @since    2.5
+ * Handles Epay payments
+ *
+ * @package     Redform
+ * @subpackage  Payment.epay
+ * @since       2.5
  */
-class PaymentEpay extends  RdfPaymentHelper
+class PaymentEpay extends RdfPaymentHelper
 {
 	/**
 	 * name of the gateway for dispatching
@@ -46,113 +48,60 @@ class PaymentEpay extends  RdfPaymentHelper
 	 */
 	public function process($request, $return_url = null, $cancel_url = null)
 	{
-		JHtml::_('behavior.mootools');
 		$document = JFactory::getDocument();
 		$document->addScript("https://ssl.ditonlinebetalingssystem.dk/integration/ewindow/paymentwindow.js");
-		$document->addScript(JURI::root(). "plugins/redform_payment/epay/js/epay.js");
 
 		$details = $this->_getSubmission($request->key);
 		$submit_key = $request->key;
 
 		$currency = RHelperCurrency::getIsoNumber($details->currency);
-		?>
-		<h3><?php echo JText::_('PLG_REDFORM_PAYMENT_EPAY_FORM_TITLE'); ?></h3>
-		<form action="https://ssl.ditonlinebetalingssystem.dk/popup/default.asp" method="post" name="ePay" target="ePay_window" id="ePay">
-		<p><?php echo $request->title; ?></p>
-		<input type="hidden" name="merchantnumber" value="<?php echo $this->params->get('EPAY_MERCHANTNUMBER'); ?>">
-		<input type="hidden" name="amount" value="<?php echo round($details->price*100); ?>">
-		<input type="hidden" name="currency" value="<?php echo $currency?>">
-		<input type="hidden" name="orderid" value="<?php echo $request->uniqueid; ?>">
-		<input type="hidden" name="user_attr_1" value="<?php echo $submit_key; ?>">
-		<input type="hidden" name="ordretext" value="">
-		<?php
-		if ($this->params->get('EPAY_CALLBACK') == "1")
-		{
-			echo '<input type="hidden" name="callbackurl" value="' . $this->getUrl('notify', $submit_key) . '">';
-		}
-		$premd5 = $currency . round($details->price*100) . $request->uniqueid  . $this->params->get('EPAY_MD5_KEY');
-		?>
-		<input type="hidden" name="accepturl" value="<?php echo $this->getUrl('notify', $submit_key); ?>">
-		<input type="hidden" name="declineurl" value="<?php echo $this->getUrl('decline', $submit_key); ?>">
-		<input type="hidden" name="ordertext" value="<?php echo $request->title; ?>">
-		<input type="hidden" name="description" value="<?php echo $request->title; ?>">
-		<input type="hidden" name="group" value="<?php echo $this->params->get('EPAY_GROUP'); ?>">
-		<input type="hidden" name="instant" value="<?php echo $this->params->get('EPAY_INSTANT_CAPTURE'); ?>">
-		<input type="hidden" name="language" value="<?php echo $this->params->get('EPAY_LANGUAGE') ?>">
-		<input type="hidden" name="authsms" value="<?php echo $this->params->get('EPAY_AUTH_SMS') ?>">
-		<input type="hidden" name="authmail" value="<?php echo $this->params->get('EPAY_AUTH_MAIL'); ?>">
-		<input type="hidden" name="windowstate" value="<?php echo $this->params->get('EPAY_WINDOW_STATE') ?>">
-		<input type="hidden" name="use3D" value="<?php echo $this->params->get('EPAY_3DSECURE') ?>">
-		<input type="hidden" name="addfee" value="<?php echo $this->params->get('EPAY_ADDFEE') ?>">
-		<input type="hidden" name="subscription" value="<?php echo $this->params->get('EPAY_SUBSCRIPTION') ?>">
-		<input type="hidden" name="md5Key" value="<?php if ($this->params->get('MD5') == 2) echo md5($premd5); ?>">
-		<?php if ($this->params->get('EPAY_CARDTYPES_0') == "0"): ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_1') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="1"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_2') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="2"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_3') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="3"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_4') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="4"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_5') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="5"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_6') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="6"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_6') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="7"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_8') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="8"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_9') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="9"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_10') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="10"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_11') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="11"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_12') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="12"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_14') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="14"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_15') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="15"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_16') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="16"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_17') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="17"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_18') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="18"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_19') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="19"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_21') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="21"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('EPAY_CARDTYPES_22') == "1"): ?>
-			<input type="hidden" name="cardtype[]" value="22"/>
-			<?php endif; ?>
-		<?php endif; ?>
 
-			<p><?php echo $this->params->get('PLG_REDFORM_EPAY_REDIRECT_TEXT', 'You will be redirected to the payment window in 5 seconds'); ?></p>
-			<input type="submit" value="<?php echo JTEXT::_('OPEN_EPAY_PAYMENT_WINDOW'); ?>">
-		</form>
-		<br>
+		$params = array(
+			'merchantnumber' => $this->params->get('EPAY_MERCHANTNUMBER'),
+			'currency' => $currency,
+			'amount' => round($details->price * 100),
+			'orderid' => $request->uniqueid,
+			'windowstate' => $this->params->get('windowstate'),
+			'paymentcollection' => $this->params->get('paymentcollection'),
+			'lockpaymentcollection' => $this->params->get('lockpaymentcollection'),
+			'language' => $this->params->get('language'),
+			'instantcapture' => $this->params->get('instantcapture'),
+			'callbackurl' => $this->getUrl('notify', $submit_key),
+			'accepturl' => $this->getUrl('notify', $submit_key),
+			'cancelurl' => $this->getUrl('cancel', $submit_key),
+			'ordertext' => $request->title,
+			'group' => $this->params->get('group'),
+			'description' => $request->title,
+			'opacity' => 50,
+		);
+
+		if ($this->params->get('paymenttype'))
+		{
+			$types = implode(',', $this->params->get('paymenttype'));
+			$params['paymenttype'] = $types;
+		}
+
+		?>
+		<div id="payment-div"></div>
+		<script>
+			paymentwindow = new PaymentWindow({
+				<?php
+
+				foreach ($params as $key => $value)
+				{
+					echo "'" . $key . "': \"" . $value . "\",\n";
+				}
+
+				if ($this->params->get('md5'))
+				{
+					$hash = md5(implode("", array_values($params)));
+					echo "'hash': \"" . $hash . "\"\n";
+				}
+				?>
+			});
+			paymentwindow.append('payment-div');
+			paymentwindow.open();
+		</script>
 		<?php
 
 		return true;
@@ -160,70 +109,91 @@ class PaymentEpay extends  RdfPaymentHelper
 
 	/**
 	 * handle the recpetion of notification
+	 *
 	 * @return bool paid status
 	 */
-  function notify()
-  {
-    $mainframe = &JFactory::getApplication();
-    $db = & JFactory::getDBO();
-    $paid = 0;
+	public function notify()
+	{
+		$mainframe = JFactory::getApplication();
+		$db = JFactory::getDBO();
+		$paid = 0;
 
-    $submit_key = JRequest::getvar('key');
-    JRequest::setVar('submit_key', $submit_key);
-    RdfHelperLog::simpleLog('EPAY NOTIFICATION RECEIVED'. ' for ' . $submit_key);
+		$submit_key = JRequest::getvar('key');
+		JRequest::setVar('submit_key', $submit_key);
+		RdfHelperLog::simpleLog('EPAY NOTIFICATION RECEIVED' . ' for ' . $submit_key);
 
-    if (JRequest::getVar('accept', 0) == 0)
-    {
-    	// payment was refused
-    	RdfHelperLog::simpleLog('EPAY NOTIFICATION PAYMENT REFUSED'. ' for ' . $submit_key);
-    	$this->writeTransaction($submit_key, JRequest::getVar('error').': '.JRequest::getVar('errortext'), $this->params->get('EPAY_INVALID_STATUS', 'FAIL'), 0);
-	    return 0;
-    }
-    // it was successull, get the details
-    $resp = array();
-    $resp[] = 'tid:'.JRequest::getVar('tid');
-    $resp[] = 'orderid:'.JRequest::getVar('orderid');
-    $resp[] = 'amount:'.JRequest::getVar('amount');
-    $resp[] = 'cur:'.JRequest::getVar('cur');
-    $resp[] = 'date:'.JRequest::getVar('date');
-    $resp[] = 'time:'.JRequest::getVar('time');
-    $resp = implode("\n", $resp);
+		if (JRequest::getVar('accept', 0) == 0)
+		{
+			// Payment was refused
+			RdfHelperLog::simpleLog('EPAY NOTIFICATION PAYMENT REFUSED' . ' for ' . $submit_key);
+			$this->writeTransaction($submit_key, JRequest::getVar('error') . ': ' . JRequest::getVar('errortext'), $this->params->get('EPAY_INVALID_STATUS', 'FAIL'), 0);
 
-    $details = $this->_getSubmission($submit_key);
+			return 0;
+		}
+
+		// It was successull, get the details
+		$resp = array();
+		$resp[] = 'tid:' . JRequest::getVar('txnid');
+		$resp[] = 'orderid:' . JRequest::getVar('orderid');
+		$resp[] = 'amount:' . JRequest::getVar('amount');
+		$resp[] = 'currency:' . JRequest::getVar('currency');
+		$resp[] = 'date:' . JRequest::getVar('date');
+		$resp[] = 'time:' . JRequest::getVar('time');
+		$resp = implode("\n", $resp);
+
+		$details = $this->_getSubmission($submit_key);
 
 		$currency = RHelperCurrency::getIsoNumber($details->currency);
 
-    if (round($details->price*100) != JRequest::getVar('amount')) {
-    	RdfHelperLog::simpleLog('EPAY NOTIFICATION PRICE MISMATCH'. ' for ' . $submit_key);
-    	$this->writeTransaction($submit_key, 'EPAY NOTIFICATION PRICE MISMATCH'."\n".$resp, $this->params->get('EPAY_INVALID_STATUS', 'FAIL'), 0);
-    	return false;
-    }
-    else {
-    	$paid = 1;
-    }
+		if (round($details->price * 100) != JRequest::getVar('amount'))
+		{
+			RdfHelperLog::simpleLog('EPAY NOTIFICATION PRICE MISMATCH' . ' for ' . $submit_key);
+			$this->writeTransaction($submit_key, 'EPAY NOTIFICATION PRICE MISMATCH' . "\n" . $resp, $this->params->get('EPAY_INVALID_STATUS', 'FAIL'), 0);
 
-    if ($currency != JRequest::getVar('cur')) {
-    	RdfHelperLog::simpleLog('EPAY NOTIFICATION CURRENCY MISMATCH'. ' for ' . $submit_key);
-    	$this->writeTransaction($submit_key, 'EPAY NOTIFICATION CURRENCY MISMATCH'."\n".$resp, $this->params->get('EPAY_INVALID_STATUS', 'FAIL'), 0);
-    	return false;
-    }
+			return false;
+		}
+		else
+		{
+			$paid = 1;
+		}
 
-    if ($this->params->get('MD5', 0) > 0)
-    {
-    	$receivedkey = JRequest::getVar('eKey');
-    	$calc = md5(JRequest::getVar('amount').JRequest::getVar('orderid').JRequest::getVar('tid').$this->params->get('EPAY_MD5_KEY'));
-    	if (strcmp($receivedkey, $calc))
-    	{
-	    	RdfHelperLog::simpleLog('EPAY NOTIFICATION MD5 KEY MISMATCH'. ' for ' . $submit_key);
-	    	$this->writeTransaction($submit_key, 'EPAY NOTIFICATION MD5 KEY MISMATCH'."\n".$resp, $this->params->get('EPAY_INVALID_STATUS', 'FAIL'), 0);
-	    	return false;
-    	}
-    }
+		if ($currency != JRequest::getVar('currency'))
+		{
+			RdfHelperLog::simpleLog('EPAY NOTIFICATION CURRENCY MISMATCH' . ' for ' . $submit_key);
+			$this->writeTransaction($submit_key, 'EPAY NOTIFICATION CURRENCY MISMATCH' . "\n" . $resp, $this->params->get('EPAY_INVALID_STATUS', 'FAIL'), 0);
+			return false;
+		}
 
-	  $this->writeTransaction($submit_key, $resp, 'SUCCESS', 1);
+		if ($this->params->get('md5', 0) > 0)
+		{
+			$params = $_GET;
+			$var = "";
 
-    return $paid;
-  }
+			$exclude = array('hash');
+
+			foreach ($params as $key => $value)
+			{
+				if (!in_array($key, $exclude))
+				{
+					$var .= $value;
+				}
+			}
+
+			$genstamp = md5($var . $this->params->get('EPAY_MD5_KEY'));
+
+			if ($genstamp != $_GET["hash"])
+			{
+				RdfHelperLog::simpleLog('EPAY NOTIFICATION MD5 KEY MISMATCH' . ' for ' . $submit_key);
+				$this->writeTransaction($submit_key, 'EPAY NOTIFICATION MD5 KEY MISMATCH' . "\n" . $resp, $this->params->get('EPAY_INVALID_STATUS', 'FAIL'), 0);
+
+				return false;
+			}
+		}
+
+		$this->writeTransaction($submit_key, $resp, 'SUCCESS', 1);
+
+		return $paid;
+	}
 
 	/**
 	 * returns state uri object (notify, cancel, etc...)
--- plugins/redform_payment/epay/js/epay.js
+++
@@ -1,5 +0,0 @@
-window.addEvent('domready', function() {
-	setTimeout(function() {
-		document.id('ePay').submit();
-	}, 5000);
-});
