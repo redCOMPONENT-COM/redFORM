0c2c1a67b3da8ff992097d389beee73cdb36c2e2
julien <jlv@jlv-solutions.com>
2014-11-15 20:50:11 +0100

    ajax postcodes/city

--- override/javascript/gasel.js
+++ override/javascript/gasel.js
@@ -9,6 +9,10 @@
 		});
 	});
 
+	var escapeRegExChars = function (value) {
+		return value.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+	}
+
 	var gasform = function(element) {
 		var form = $(element.form);
 
@@ -39,6 +43,33 @@
 
 			form.find('input[name=rategroups]').val(rates);
 		};
+
+		$('.gasel-zip').autocomplete({
+			serviceUrl: "index.php?option=com_ajax&plugin=gaselkmd&format=json&function=postcode",
+			paramName: 'q',
+			minChars: 1,
+			transformResult: function(jsonresp){
+				var response = JSON.parse(jsonresp);
+
+				if (!response.data[0]) {
+					return;
+				}
+
+				return {
+					suggestions: $.map(response.data[0], function(dataItem) {
+						return {value: dataItem.nr, data: dataItem};
+					})
+				};
+			},
+			formatResult : function (suggestion, currentValue) {
+				var pattern = '(' + escapeRegExChars(currentValue) + ')';
+
+				return suggestion.value.replace(new RegExp(pattern, 'gi'), '<strong>$1<\/strong>') + ' ' + suggestion.data.navn;
+			},
+			onSelect : function(suggestion) {
+				form.find('.gasel-city').val(suggestion.data.navn);
+			}
+		});
 
 		form.find('input.esGas, input.gasel-elec-radio').click(updateRategroups);
 		updateRategroups();
---
+++ plugins/ajax/gaselkmd/gaselkmd.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @package     Redform.Plugin
+ * @subpackage  Redform.gaselkmd
+ *
+ * @copyright   Copyright (C) 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
+ */
+
+defined('JPATH_BASE') or die;
+
+// Import library dependencies
+jimport('joomla.plugin.plugin');
+
+/**
+ * Handle ajax queries for gasel
+ *
+ * @package     Redform.Plugin
+ * @subpackage  Redform.gaselkmd
+ * @since       3.0
+ */
+class plgAjaxGaselkmd extends JPlugin
+{
+	/**
+	 * Callback
+	 *
+	 * @return mixed
+	 */
+	public function onAjaxGaselkmd()
+	{
+		$function = JFactory::getApplication()->input->get('function');
+
+		if (method_exists($this, $function))
+		{
+			return $this->{$function}();
+		}
+	}
+
+	/**
+	 * Get and return postcodes
+	 *
+	 * @return array
+	 *
+	 * @throws RuntimeException
+	 */
+	private function postcode()
+	{
+		$code = JFactory::getApplication()->input->get('q');
+
+		$ch = curl_init();
+		$url = "http://geo.oiorest.dk/postnumre.json?q=" . $code;
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
+		curl_setopt($ch, CURLOPT_POST, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$json = curl_exec($ch);
+
+		if (curl_exec($ch) === false)
+		{
+			throw new RuntimeException('Curl error: ' . curl_error($ch));
+		}
+
+		$data = json_decode($json);
+
+		return $data;
+	}
+}
