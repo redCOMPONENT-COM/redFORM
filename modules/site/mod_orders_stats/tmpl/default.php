5a557562c589a4f0a5d3989b4387c91a5d86bf58
julien vonthron <julien@redweb.dk>
2014-12-20 13:15:41 +0100

    added translations

--- modules/site/mod_orders_stats/tmpl/default.php
+++ modules/site/mod_orders_stats/tmpl/default.php
@@ -21,9 +21,21 @@ $doc->addStyleSheet(JURI::base() . 'modules/mod_orders_stats/media/css/mod_order
 <div role="tabpanel">
 	<!-- Nav tabs -->
 	<ul class="nav nav-tabs" role="tablist">
-		<li role="presentation" class="active"><a href="#area1tab1" aria-controls="home" role="tab" data-toggle="tab">Top sælgere</a></li>
-		<li role="presentation"><a href="#area1tab2" aria-controls="profile" role="tab" data-toggle="tab">Dagens salg</a></li>
-		<li role="presentation"><a href="#area1tab3" aria-controls="messages" role="tab" data-toggle="tab">Diagram</a></li>
+		<li role="presentation" class="active">
+			<a href="#area1tab1" aria-controls="home" role="tab" data-toggle="tab">
+				<?php echo JText::_('MOD_ORDERS_STATS_TOP_SALESMEN'); ?>
+			</a>
+		</li>
+		<li role="presentation">
+			<a href="#area1tab2" aria-controls="profile" role="tab" data-toggle="tab">
+				<?php echo JText::_('MOD_ORDERS_STATS_COMPANIES'); ?>
+			</a>
+		</li>
+		<li role="presentation">
+			<a href="#area1tab3" aria-controls="messages" role="tab" data-toggle="tab">
+				<?php echo JText::_('MOD_ORDERS_STATS_CHART'); ?>
+			</a>
+		</li>
 	</ul>
 
 	<!-- Tab panes -->
@@ -32,11 +44,11 @@ $doc->addStyleSheet(JURI::base() . 'modules/mod_orders_stats/media/css/mod_order
 			<table class="table table-striped">
 				<thead>
 				<tr>
-					<th colspan="2">Sælgernavn</th>
-					<th>Bureau</th>
-					<th>El kunder</th>
-					<th>Gas kunder</th>
-					<th>Samlet</th>
+					<th colspan="2"><?php echo JText::_('MOD_ORDERS_STATS_SALESMAN'); ?></th>
+					<th><?php echo JText::_('MOD_ORDERS_STATS_COMPANY'); ?></th>
+					<th><?php echo JText::_('MOD_ORDERS_STATS_EL_ORDERS'); ?></th>
+					<th><?php echo JText::_('MOD_ORDERS_STATS_GAS_ORDERS'); ?></th>
+					<th><?php echo JText::_('MOD_ORDERS_STATS_TOTAL_ORDERS'); ?></th>
 				</tr>
 				</thead>
 				<tbody>
@@ -58,10 +70,10 @@ $doc->addStyleSheet(JURI::base() . 'modules/mod_orders_stats/media/css/mod_order
 			<table class="table table-striped">
 				<thead>
 				<tr>
-					<th colspan="2">Bureau</th>
-					<th>El kunder</th>
-					<th>Gas kunder</th>
-					<th>Samlet</th>
+					<th colspan="2"><?php echo JText::_('MOD_ORDERS_STATS_COMPANY'); ?></th>
+					<th><?php echo JText::_('MOD_ORDERS_STATS_EL_ORDERS'); ?></th>
+					<th><?php echo JText::_('MOD_ORDERS_STATS_GAS_ORDERS'); ?></th>
+					<th><?php echo JText::_('MOD_ORDERS_STATS_TOTAL_ORDERS'); ?></th>
 				</tr>
 				</thead>
 				<tbody>
