b3a9bd585f897e8133ead23e326e341d5f7e086d
julien <jlv@jlv-solutions.com>
2015-08-14 18:10:54 +0200

    Make sure form table has all fields

--- component/admin/controllers/form.php
+++ component/admin/controllers/form.php
@@ -56,4 +56,29 @@ class RedformControllerForm extends RdfControllerForm
 
 		$app->close();
 	}
+
+	/**
+	 * Method to edit an existing record.
+	 *
+	 * @param   string  $key     The name of the primary key of the URL variable.
+	 * @param   string  $urlVar  The name of the URL variable if different from the primary key
+	 *                         (sometimes required to avoid router collisions).
+	 *
+	 * @return  boolean  True if access level check and checkout passes, false otherwise.
+	 */
+	public function edit($key = null, $urlVar = null)
+	{
+		$cid   = $this->input->post->get('cid', array(), 'array');
+
+		// Get the previous record id (if any) and the current record id.
+		$recordId = (int) (count($cid) ? $cid[0] : $this->input->getInt('id'));
+
+		if ($recordId)
+		{
+			$model = $this->getModel();
+			$model->checkFields($recordId);
+		}
+
+		return parent::edit($key, $urlVar);
+	}
 }
