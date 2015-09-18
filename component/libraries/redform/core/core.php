2575b306fe07ef321b53e13ae21c57c639e5ba65
julien <jlv@jlv-solutions.com>
2015-09-18 16:56:23 +0200

    [bug] fixed core getSubmissionsPaymentRequests for paid status

--- component/libraries/redform/core/core.php
+++ component/libraries/redform/core/core.php
@@ -1121,10 +1121,15 @@ class RdfCore extends JObject
 		$db = JFactory::getDbo();
 		$query = $db->getQuery(true);
 
-		$query->select('*')
-			->from('#__rwf_payment_request')
-			->where('submission_id IN (' . implode(', ', $sids) . ')')
-			->order('id DESC');
+		$query->select('pr.*')
+			->select('CASE WHEN p.id IS NOT NULL THEN 1 ELSE 0 END AS paid')
+			->from('#__rwf_payment_request AS pr')
+			->leftJoin('#__rwf_cart_item AS ci ON ci.payment_request_id = pr.id')
+			->leftJoin('#__rwf_cart AS c ON c.id = ci.cart_id')
+			->leftJoin('#__rwf_payment AS p ON p.cart_id = c.id AND p.paid = 1')
+			->where('pr.submission_id IN (' . implode(', ', $sids) . ')')
+			->order('pr.id DESC')
+			->group('pr.id');
 
 		$db->setQuery($query);
 		$paymentRequests = $db->loadObjectList();
