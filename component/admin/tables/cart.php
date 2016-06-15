430070552f50bd16ff3e25b07a17a73b0b72dc15
julien <jlv@jlv-solutions.com>
2016-06-15 14:29:04 +0200

    [fix] delete unpaid pr when turning a submission

--- component/admin/tables/cart.php
+++ component/admin/tables/cart.php
@@ -24,4 +24,26 @@ class RedformTableCart extends RTable
 	 * @var  string
 	 */
 	protected $_tableName = 'rwf_cart';
+
+	/**
+	 * Called after delete().
+	 *
+	 * @param   mixed  $pk  An optional primary key value to delete.  If not set the instance property value is used.
+	 *
+	 * @return  boolean  True on success.
+	 */
+	protected function afterDelete($pk = null)
+	{
+		$pk = $pk ?: $this->id;
+
+		$db = $this->_db;
+		$query = $db->getQuery(true)
+			->delete('#__rwf_cart_item')
+			->where('cart_id = ' . $pk);
+
+		$db->setQuery($query);
+		$db->execute();
+
+		return parent::afterDelete($pk);
+	}
 }
