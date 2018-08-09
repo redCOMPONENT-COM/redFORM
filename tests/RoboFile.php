a6cfcf95994718798fbd0dcdb05da67e1ea4525c
Nhung <nhung@redweb.dk>
2018-08-09 16:11:43 +0700

    send email fail

--- tests/RoboFile.php
+++ tests/RoboFile.php
@@ -485,7 +485,7 @@ class RoboFile extends \Robo\Tasks
 		 * When joomla Staging branch has a bug you can uncomment the following line as a tmp fix for the tests layer.
 		 * Use as $version value the latest tagged stable version at: https://github.com/joomla/joomla-cms/releases
 		 */
-		$version = '3.7.2';
+		$version = '3.8.11';
 
 		$this->_exec("git clone -b $version --single-branch --depth 1 https://github.com/joomla/joomla-cms.git joomla-cms3");
 
@@ -686,4 +686,104 @@ class RoboFile extends \Robo\Tasks
 			}
 		}
 	}
+	
+	/**
+	 * Sends the build report error back to Slack
+	 *
+	 * @param   string $cloudinaryName Cloudinary cloud name
+	 * @param   string $cloudinaryApiKey Cloudinary API key
+	 * @param   string $cloudinaryApiSecret Cloudinary API secret
+	 * @param   string $githubRepository GitHub repository (owner/repo)
+	 * @param   string $githubPRNo GitHub PR #
+	 * @param   string $slackWebhook Slack Webhook URL
+	 * @param   string $slackChannel Slack channel
+	 * @param   string $buildURL Build URL
+	 *
+	 * @return  void
+	 *
+	 * @since   5.1
+	 */
+	public function sendSystemBuildReportErrorSlack($cloudinaryName, $cloudinaryApiKey, $cloudinaryApiSecret, $githubRepository, $githubPRNo, $slackWebhook, $slackChannel, $buildURL = '')
+	{
+		$errorSelenium = true;
+		$reportError = false;
+		$reportFile = 'selenium.log';
+		$errorLog = 'Selenium log:' . chr(10) . chr(10);
+		
+		// Loop through Codeception snapshots
+		if (file_exists('_output') && $handler = opendir('_output'))
+		{
+			$reportFile = '_output';
+			$errorLog = 'Codeception tap log:' . chr(10) . chr(10);
+			$errorSelenium = false;
+			$this->_exec($reportFile);
+		}
+		
+		if (file_exists($reportFile))
+		{
+			
+			if ($reportFile)
+			{
+				$errorLog .= file_get_contents($reportFile, null, null, 15);
+			}
+			
+			if (!$errorSelenium)
+			{
+				$handler = opendir('_output');
+				$errorImage = '';
+				
+				while (!$reportError && false !== ($errorSnapshot = readdir($handler)))
+				{
+					// Avoid sending system files or html files
+					if (!('png' === pathinfo($errorSnapshot, PATHINFO_EXTENSION)))
+					{
+						continue;
+					}
+					
+					$reportError = true;
+					
+					Cloudinary::config(
+						array(
+							'cloud_name' => $cloudinaryName,
+							'api_key' => $cloudinaryApiKey,
+							'api_secret' => $cloudinaryApiSecret
+						)
+					);
+					
+					$result = \Cloudinary\Uploader::upload(realpath(dirname(__FILE__) . '/_output/' . $errorSnapshot));
+					
+					$this->say($errorSnapshot . 'Image sent');
+					
+					$errorLog .=  $result['secure_url'];
+					
+				}
+			}
+			
+			if ($reportError || $errorSelenium)
+			{
+				// Sends the error report to Slack
+				$reportingTask = $this->taskReporting()
+					->setCloudinaryCloudName($cloudinaryName)
+					->setCloudinaryApiKey($cloudinaryApiKey)
+					->setCloudinaryApiSecret($cloudinaryApiSecret)
+					->setGithubRepo($githubRepository)
+					->setGithubPR($githubPRNo)
+					->setBuildURL($buildURL . 'display/redirect')
+					->setSlackWebhook($slackWebhook)
+					->setSlackChannel($slackChannel)
+					->setTapLog($errorLog);
+				
+				if (!empty($errorImage))
+				{
+					$reportingTask->setImagesToUpload($errorLog)
+						->publishCloudinaryImages();
+				}
+				
+				$reportingTask->publishBuildReportToSlack()
+					->run()
+					->stopOnFail();
+			}
+		}
+	}
+	
 }
