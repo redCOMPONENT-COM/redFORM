d745187adc7333a7a54eee2440f8bfe036f04d2c
Nhung <nhung@redweb.dk>
2018-08-10 17:19:01 +0700

    copy redform

--- build/test-setup.sh
+++ build/test-setup.sh
@@ -40,8 +40,9 @@ mv gulp-config.json.jenkins.dist gulp-config.json
 git submodule update --init --recursive
 
 gulp release --skip-version
-cd ../tests
+cd ..
 cp /tests/www/tests/releases-redform/redform.zip .
+ls
 zip --symlinks -r gulp-release.zip /tests/www/tests/releases-redform > output.log 2>&1
 
 # back to tests for run codeception
