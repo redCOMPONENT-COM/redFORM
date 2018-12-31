820fe91b7f3ccb2d250f4b4bb7e33ca6d9c73459
Julien Vonthron <julien.vonthron@gmail.com>
2018-12-31 17:56:41 +0100

    [imp] Added rdfif with conditions as plugins to replace blocks conditionally #REDFORM-205

---
+++ component/libraries/redform/src/Plugin/AbstractReplaceconditionPlugin.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @package    Redform.Library
+ *
+ * @copyright  Copyright (C) 2009 - 2018 redCOMPONENT.com. All rights reserved.
+ * @license    GNU General Public License version 2 or later, see LICENSE.
+ */
+
+namespace Redform\Plugin;
+
+use Joomla\CMS\Language\Text;
+
+defined('_JEXEC') or die;
+
+/**
+ * Tag container
+ *
+ * @package  Redform.Library
+ * @since    __deploy_version__
+ */
+abstract class AbstractReplaceconditionPlugin extends \JPlugin
+{
+	protected $autoloadLanguage = true;
+
+	protected $name;
+
+	/**
+	 * Add supported condition(s)
+	 *
+	 * @param   []  $conditions  conditions
+	 *
+	 * @return void
+	 */
+	public function onRedformGetConditions(&$conditions)
+	{
+		$conditions[] = [
+			'name'        => $this->name,
+			'usage'       => Text::_('PLG_REDFORM_REPLACECONDITION_' . $this->name . '_USAGE'),
+			'description' => Text::_('PLG_REDFORM_REPLACECONDITION_' . $this->name . '_DESCRIPTION'),
+		];
+	}
+
+	/**
+	 * Process condition
+	 *
+	 * @param   array        $condition  condition parts
+	 * @param   \RdfAnswers  $answers    answers from submission
+	 * @param   boolean      $isValid    return true if valid, else false
+	 *
+	 * @return void
+	 */
+	abstract public function onRedformProcessReplaceCondition($condition, \RdfAnswers $answers, &$isValid);
+}
---
+++ component/plugins/redform_replacecondition/isset/isset.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package    Redform.plugins
+ * @copyright  Copyright (C) 2012 - 2018 redCOMPONENT.com. All rights reserved.
+ * @license    GNU General Public License version 2 or later, see LICENSE.
+ */
+
+use Redform\Plugin\AbstractReplaceconditionPlugin;
+
+defined('_JEXEC') or die;
+
+jimport('joomla.plugin.plugin');
+
+/**
+ * redFORM Isset Replace condition plugin
+ *
+ * @package  Redform.plugins
+ * @since    __deploy_version__
+ */
+class PlgRedform_ReplaceconditionIsset extends AbstractReplaceconditionPlugin
+{
+	/**
+	 * @var string
+	 */
+	protected $name = 'isset';
+
+	/**
+	 * Process condition
+	 *
+	 * @param   array        $condition  condition parts
+	 * @param   \RdfAnswers  $answers    answers from submission
+	 * @param   boolean      $isValid    return true if valid, else false
+	 *
+	 * @return void
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function onRedformProcessReplaceCondition($condition, \RdfAnswers $answers, &$isValid)
+	{
+		if ($condition[0] !== $this->name)
+		{
+			return;
+		}
+
+		if (!strstr($condition[1], 'field_'))
+		{
+			throw new \InvalidArgumentException('Invalid argument for isset condition');
+		}
+
+		$fieldId = substr($condition[1], strlen('field_'));
+
+		foreach ($answers->getFields() as $field)
+		{
+			if ($field->fieldId == $fieldId)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
