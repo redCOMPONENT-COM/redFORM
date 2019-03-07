1b8aea046f36282ea83f38c481227f590d1d208f
atabegruslan <ruslan_aliyev_@hotmail.com>
2019-03-07 17:55:43 +0700

    more edits

--- plugins/redform_captcha/recaptcha/recaptcha.php
+++ plugins/redform_captcha/recaptcha/recaptcha.php
@@ -22,71 +22,71 @@ include_once 'vendor/autoload.php';
  */
 class PlgRedform_captchaRecaptcha extends JPlugin
 {
-    private $version;
+	private $version;
 
-    private $publicKey;
+	private $publicKey;
 
-    private $privateKey;
+	private $privateKey;
 
-    private $apiScript = 'https://www.google.com/recaptcha/api.js';
+	private $apiScript = 'https://www.google.com/recaptcha/api.js';
 
-    private $expectedAction;
+	private $expectedAction;
 
-    private $thresholdScore;
+	private $thresholdScore;
 
-    private $responseElementId = 'g-recaptcha-response';
+	private $responseElementId = 'g-recaptcha-response';
 
-    /**
-     * Constructor
-     *
-     * @param   object  &$subject  The object to observe
-     * @param   array   $config    An optional associative array of configuration settings.
-     *                             Recognized key values include 'name', 'group', 'params', 'language'
-     *                             (this list is not meant to be comprehensive).
-     */
-    public function __construct(&$subject, $config = array())
-    {
-        parent::__construct($subject, $config);
-        $this->loadLanguage();
+	/**
+	 * Constructor
+	 *
+	 * @param   object  &$subject  The object to observe
+	 * @param   array   $config    An optional associative array of configuration settings.
+	 *                             Recognized key values include 'name', 'group', 'params', 'language'
+	 *                             (this list is not meant to be comprehensive).
+	 */
+	public function __construct(&$subject, $config = array())
+	{
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
 
-        $this->version = $this->params->get('version');
+		$this->version = $this->params->get('version');
 
-        if ($this->version == 2)
-        {
-            $this->publicKey  = $this->params->get('public_key_v2');
-            $this->privateKey = $this->params->get('private_key_v2');
-        }
+		if ($this->version == 2)
+		{
+			$this->publicKey  = $this->params->get('public_key_v2');
+			$this->privateKey = $this->params->get('private_key_v2');
+		}
 
-        if ($this->version == 3)
-        {
-            $this->publicKey      = $this->params->get('public_key_v3');
-            $this->privateKey     = $this->params->get('private_key_v3');
-            $this->thresholdScore = $this->params->get('min_accepted_score_v3');
-            $this->expectedAction = $this->params->get('expected_action_v3');
-        }
-    }
+		if ($this->version == 3)
+		{
+			$this->publicKey      = $this->params->get('public_key_v3');
+			$this->privateKey     = $this->params->get('private_key_v3');
+			$this->thresholdScore = $this->params->get('min_accepted_score_v3');
+			$this->expectedAction = $this->params->get('expected_action_v3');
+		}
+	}
 
-    /**
-     * onGetCaptchaField trigger
-     *
-     * @param   string  &$text  text to modify
-     *
-     * @return bool
-     */
-    public function onGetCaptchaField(&$text)
-    {
-        $document                = JFactory::getDocument();
-        $differentiator          = rand();
-        $responseElementIdUnique = $this->responseElementId . $differentiator;
+	/**
+	 * onGetCaptchaField trigger
+	 *
+	 * @param   string  &$text  text to modify
+	 *
+	 * @return bool
+	 */
+	public function onGetCaptchaField(&$text)
+	{
+		$document                = JFactory::getDocument();
+		$differentiator          = rand();
+		$responseElementIdUnique = $this->responseElementId . $differentiator;
 
-        if ($this->version == 2)
-        {
-            $document->addScript($this->apiScript);
-        }
+		if ($this->version == 2)
+		{
+			$document->addScript($this->apiScript);
+		}
 
-        if ($this->version == 3)
-        {
-            $document->addScript($this->apiScript . '?render=' . $this->publicKey);
+		if ($this->version == 3)
+		{
+			$document->addScript($this->apiScript . '?render=' . $this->publicKey);
 
             $document->addScriptDeclaration('
 				grecaptcha.ready(function() 
@@ -97,63 +97,63 @@ class PlgRedform_captchaRecaptcha extends JPlugin
 					});
 				});
 			');
-        }
+		}
 
-        $attributes = array();
+		$attributes = array();
 
-        $attributes['data-sitekey'] = $this->publicKey;
+		$attributes['data-sitekey'] = $this->publicKey;
 
-        if ($this->params->get('theme'))
-        {
-            $attributes['data-theme'] = $this->params->get('theme');
-        }
+		if ($this->params->get('theme'))
+		{
+			$attributes['data-theme'] = $this->params->get('theme');
+		}
 
-        if ($this->params->get('size'))
-        {
-            $attributes['data-size'] = $this->params->get('size');
-        }
+		if ($this->params->get('size'))
+		{
+			$attributes['data-size'] = $this->params->get('size');
+		}
 
-        if ($this->version == 2)
-        {
-            $text = '<div class="g-recaptcha"' . JArrayHelper::toString($attributes, '=', ' ') . '></div>';
-        }
+		if ($this->version == 2)
+		{
+			$text = '<div class="g-recaptcha"' . JArrayHelper::toString($attributes, '=', ' ') . '></div>';
+		}
 
-        if ($this->version == 3)
-        {
-            $text = '<input type="hidden" id="' . $responseElementIdUnique . '" name="' . $responseElementIdUnique . '">';
+		if ($this->version == 3)
+		{
+			$text = '<input type="hidden" id="' . $responseElementIdUnique . '" name="' . $responseElementIdUnique . '">';
 
-            $text .= '<input type="hidden" name="differentiator" value="' . $differentiator . '">';
-        }
+			$text .= '<input type="hidden" name="differentiator" value="' . $differentiator . '">';
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * onCheckCaptcha trigger
-     *
-     * @param   bool  &$result  result
-     *
-     * @return bool
-     */
-    public function onCheckCaptcha(&$result)
-    {
-        require_once 'vendor/autoload.php';
+	/**
+	 * onCheckCaptcha trigger
+	 *
+	 * @param   bool  &$result  result
+	 *
+	 * @return bool
+	 */
+	public function onCheckCaptcha(&$result)
+	{
+		require_once 'vendor/autoload.php';
 
-        $app                = JFactory::getApplication();
-        $differentiator     = $app->input->get('differentiator');
-        $gRecaptchaResponse = $app->input->get($this->responseElementId.$differentiator);
+		$app                = JFactory::getApplication();
+		$differentiator     = $app->input->get('differentiator');
+		$gRecaptchaResponse = $app->input->get($this->responseElementId . $differentiator);
 
-        $recaptcha = new \ReCaptcha\ReCaptcha($this->privateKey);
+		$recaptcha = new \ReCaptcha\ReCaptcha($this->privateKey);
 
-        if ($this->version == 3)
-        {
-            $recaptcha
-                ->setExpectedHostname($_SERVER['SERVER_NAME'])
-                ->setExpectedAction($this->expectedAction)
-                ->setScoreThreshold($this->thresholdScore);
-        }
+		if ($this->version == 3)
+		{
+			$recaptcha
+				->setExpectedHostname($_SERVER['SERVER_NAME'])
+				->setExpectedAction($this->expectedAction)
+				->setScoreThreshold($this->thresholdScore);
+		}
 
-        $resp = $recaptcha->verify($gRecaptchaResponse, $_SERVER["REMOTE_ADDR"]);
+		$resp = $recaptcha->verify($gRecaptchaResponse, $_SERVER["REMOTE_ADDR"]);
 
 //		Example of $resp
 //
@@ -168,8 +168,8 @@ class PlgRedform_captchaRecaptcha extends JPlugin
 //			["action":"ReCaptcha\Response":private]=> string(8) "homepage"
 //		}
 
-        $result = $resp->isSuccess();
+		$result = $resp->isSuccess();
 
-        return $result;
-    }
+		return $result;
+	}
 }
