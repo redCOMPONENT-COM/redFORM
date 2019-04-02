fc76f4b177a3f4a58acf4b848d13a981e1858f48
PhamHanh96 <hanhhana041096@gmail.com>
2019-04-02 14:19:51 +0700

    Submit form missing email on frontend

--- tests/acceptance/frontend/DisplayFormOnFrontendCest.php
+++ tests/acceptance/frontend/DisplayFormOnFrontendCest.php
@@ -193,4 +193,39 @@ class DisplayFormOnFrontendCest
 		$I->wantTo('Check form display in frontend');
 		$I->checkFormInFrontend($this->menuTitle, $this->fillForm);
 	}
+
+	/**
+	 * @param DisplayFormOnFrontendSteps $I
+	 * @param                            $scenario
+	 * @throws Exception
+	 */
+	public function submitFormMissingEmail(DisplayFormOnFrontendSteps $I, $scenario)
+	{
+		$I = new DisplayFormOnFrontendSteps($scenario);
+		$I->wantTo('Create new article');
+		$I->createNewArticle($this->paramsForm['name'], $this->articlesTitle, $scenario);
+		$I->wantTo('Create new menu items');
+		$I->createNewMenuItem($this->articlesTitle, $this->articles, $this->menuTitle, $this->menuItemType, 'Main Menu');
+		$I->wantToTest('Submit form with missing email');
+		$I->submitFormMissingEmail($this->menuTitle, $this->fillForm);
+	}
+
+	/**
+	 * @param DisplayFormOnFrontendSteps $I
+	 * @param                            $scenario
+	 * @throws Exception
+	 */
+	public function clearAll(DisplayFormOnFrontendSteps $I, $scenario)
+	{
+		$I->wantTo('Clear up');
+		$I = new AddAFormSteps($scenario);
+		$I->deleteForm($this->paramsForm['name']);
+
+		$I = new AddAFieldSteps($scenario);
+		$I->deleteField($this->nameField['name']);
+		$I->deleteField($this->emailField['name']);
+		$I->deleteField($this->telephoneField['name']);
+		$I->deleteField($this->noteField['name']);
+	}
+
 }
