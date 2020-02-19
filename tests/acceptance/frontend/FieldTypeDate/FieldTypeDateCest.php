6bc0d6acb1b6f9fd5a1a2c8d5a622c565275c248
unknown <trang.nguyen@redweb.dk>
2020-02-19 10:02:11 +0700

    Edit style

--- tests/acceptance/frontend/FieldTypeDate/FieldTypeDateCest.php
+++ tests/acceptance/frontend/FieldTypeDate/FieldTypeDateCest.php
@@ -2,7 +2,7 @@
 /**
  * @package     redFORM
  * @subpackage  Cest
- * @copyright   Copyright (C) 2008 - 2019 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2008 - 2020 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -134,7 +134,6 @@ class FieldTypeDateCest
 				'section_2'         => 'general',
 				'required'          => 'Yes'
 			];
-
 
 		$this->articlesTitle        = $this->faker->bothify('Article ?##?');
 		$this->articles             = 'Single Article';
